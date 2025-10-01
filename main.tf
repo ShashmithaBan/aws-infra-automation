852850312113a8bba6d9cd3fc91ac63a39faf7e8
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2025-10-01 22:30:02 +0530

    Added the Route table  module

--- main.tf
+++ main.tf
@@ -3,9 +3,22 @@ module "vpc" {
   cidr_block = var.cidr_block
 }
 
+module "igw" {
+  source = "./modules/Internet_getway"
+  vpc_id = module.vpc.vpc_id
+  
+}
+
 module "subnet" {
   source     = "./modules/subnet"
   vpc_id     = module.vpc.vpc_id
   cidr_block = var.cidr_block
   
+}
+
+module "route_table" {
+  source    = "./modules/route_table"
+  vpc_id    = module.vpc.vpc_id
+  igw_id    = module.igw.igw_id
+  subnet_id = module.subnet.subnet_id
 }
--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,6 +1,8 @@
 resource "aws_subnet" "subnet" {
   vpc_id     = var.vpc_id
   cidr_block = var.cidr_block
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "Terraform-Subnet"
